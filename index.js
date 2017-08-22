d34e536960c722a5249e7c62710fa1eee1a7533d
GreenNun <stas909@gmail.com>
2017-08-23 00:29:27 +0300

    draft

--- index.js
+++ index.js
@@ -50,24 +50,47 @@ MyForm = {
             errorFields: []
         };
 
-        var fio = document.getElementsByName("fio")[0].value;
-        var email = document.getElementsByName("email")[0].value;
-        var phone = document.getElementsByName("phone")[0].value;
+        var fio = document.getElementsByName("fio")[0];
+        var email = document.getElementsByName("email")[0];
+        var phone = document.getElementsByName("phone")[0];
 
-        if (fio.trim().replace("  ", " ").split(" ").length !== 3) {
+        if (fio.value.trim().replace("  ", " ").split(" ").length !== 3) {
             result.isValid = false;
             result.errorFields.push('fio');
+            fio.classList.add("error");
+        } else {
+            fio.classList.remove("error");
         }
 
-        // if (email.match(new RegExp('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(ya.ru|yandex.ru|yandex.ua|yandex.by|yandex.kz|yandex.com'))) {
-        //     result.isValid = false;
-        //     result.errorFields.push('email');
-        // }
+        if (!email.value.match(new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(ya.ru|yandex.ru|yandex.ua|yandex.by|yandex.kz|yandex.com)$/i))) {
+            result.isValid = false;
+            result.errorFields.push('email');
+            email.classList.add("error");
+        } else {
+            email.classList.remove("error");
+        }
 
-        // if (email.match(new RegExp('\+7\(\d{3}\)\d{3}-\d{2}-\d{2}'))) {
-        //     result.isValid = false;
-        //     result.errorFields.push('phone');
-        // }
+        if (!phone.value.match(new RegExp(/^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/)) && !checkSum(phone.value, 30)) {
+            result.isValid = false;
+            result.errorFields.push('phone');
+            phone.classList.add("error");
+        } else {
+            phone.classList.remove("error");
+        }
+
+        function checkSum(phone, max) {
+            var sum = 0;
+            for (var i = 0; i < phone.length; i++) {
+                var symbol = phone.charAt(i);
+                var number = parseInt(symbol);
+                if (!isNaN(number)) {
+                    sum += number;
+                    if (sum > max)
+                        return false;
+                }
+            }
+            return true;
+        }
 
         return result;
     },
@@ -84,12 +107,49 @@ MyForm = {
         document.getElementsByName("phone")[0].value = data.phone;
     },
     submit: function () {
+        document.getElementById("resultContainer").innerHTML = '';
+        document.getElementById("resultContainer").classList.remove("success");
+        document.getElementById("resultContainer").classList.remove("error");
+        document.getElementById("resultContainer").classList.remove("progress");
+        if (this.validate().isValid) {
+            document.getElementById("submitButton").disabled = true;
+            MyForm.setAction();
+            $.ajax({
+                url: document.getElementById("myForm").action,
+                type: "GET",
+                success: function (response) {
+                    switch (response.status) {
+                        case 'progress':
+                            document.getElementById("resultContainer").classList.add("progress");
+                            document.getElementById("resultContainer").innerHTML = "Loading...";
+                            setTimeout(function () {
+                                MyForm.submit();
+                            }, response.timeout);
+                            break;
+                        case 'error':
+                            document.getElementById("resultContainer").classList.add("error");
+                            document.getElementById("resultContainer").innerHTML = response.reason;
+                            document.getElementById("submitButton").disabled = false;
+                            break;
+                        case 'success':
+                            document.getElementById("resultContainer").classList.add("success");
+                            document.getElementById("resultContainer").innerHTML = response.result;
+                            document.getElementById("submitButton").disabled = false;
+                            break;
+                    }
+                }
+            });
+        }
 
+        return false;
+    },
+    setAction: function () {
+        document.getElementById("myForm").action = ["json/progress.json", "json/success.json", "json/error.json"][Math.floor(Math.random() * 3)];
     }
 };
 
 MyForm.setData({
-    fio: 'abc',
-    email: 'mqlo',
+    fio: 'abc a a',
+    email: 'mqlo@ya.ru',
     phone: '123'
 });
